441122257d8e00bc5249b937ffe29451a628d84d
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2025-02-25 11:17:37 -0300

    first commit

---
+++ ArtboxSnapshot/src/main/java/ArtBoxSnapShot/ArtboxSnapshot/ArtboxSnapshotApplication.java
@@ -0,0 +1,13 @@
+package ArtBoxSnapShot.ArtboxSnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtboxSnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtboxSnapshotApplication.class, args);
+	}
+
+}
---
+++ ArtboxSnapshot/src/test/java/ArtBoxSnapShot/ArtboxSnapshot/ArtboxSnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package ArtBoxSnapShot.ArtboxSnapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtboxSnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
