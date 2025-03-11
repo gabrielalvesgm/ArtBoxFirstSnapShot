6bd480ba090e5ff149dd9aa12939c42b37b75ddd
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2025-03-11 13:27:14 -0300

    Implement Client endpoints and service methods
    - Added createClient and findClientByCpfCnpj methods in ClientService.
    - Implemented POST and GET endpoints in ClientController for client creation and retrieval by CPF.
    - Updated SecurityConfig to disable security for development purposes.

---
+++ ArtboxSnapshot/src/main/java/ArtBoxSnapShot/ArtboxSnapshot/controller/ClientController.java
@@ -0,0 +1,37 @@
+package ArtBoxSnapShot.ArtboxSnapshot.controller;
+
+
+import ArtBoxSnapShot.ArtboxSnapshot.dto.ClientDTO;
+import ArtBoxSnapShot.ArtboxSnapshot.model.Client;
+import ArtBoxSnapShot.ArtboxSnapshot.service.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("clients")
+public class ClienteController {
+
+    private final ClientService clientService;
+
+    public ClienteController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    //Endpoint to create a new Client
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody ClientDTO clientDTO) {
+        Client client = clientService.createClient(clientDTO);
+        return ResponseEntity.ok(client);
+    }
+
+    //Endpoint to get a client by his CPF
+    @GetMapping("/cpfCnpj/{cpfCnpj}")
+    public ResponseEntity<Client> getClientByCpfCnpj(@PathVariable String cpfCnpj) {
+        Optional<Client> client = clientService.getClientByCpfCnpj(cpfCnpj);
+        return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+}
--- ArtboxSnapshot/src/main/java/ArtBoxSnapShot/ArtboxSnapshot/controller/ClienteController.java
+++
@@ -1,4 +0,0 @@
-package ArtBoxSnapShot.ArtboxSnapshot.controller;
-
-public class ClienteController {
-}
