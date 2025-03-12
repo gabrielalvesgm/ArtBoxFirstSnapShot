9fa27fb68bc1c4124ac51ff448929f135d4ce740
Luan <rafawow2019@gmail.com>
2025-03-11 22:49:29 -0300

    Implement Paged & parametrized client Endpoint
     - Updated ClientRepository to include findClients
     - Add constructor To ClientDTO
     - Add findClients logic to ClientService
     - Add findClients endpoint to ClientController

--- ArtboxSnapshot/src/main/java/ArtBoxSnapShot/ArtboxSnapshot/controller/ClientController.java
+++ ArtboxSnapshot/src/main/java/ArtBoxSnapShot/ArtboxSnapshot/controller/ClientController.java
@@ -5,6 +5,7 @@ package ArtBoxSnapShot.ArtboxSnapshot.controller;
 import ArtBoxSnapShot.ArtboxSnapshot.dto.ClientDTO;
 import ArtBoxSnapShot.ArtboxSnapshot.model.Client;
 import ArtBoxSnapShot.ArtboxSnapshot.service.ClientService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,13 +27,31 @@ public class ClientController {
         Client client = clientService.createClient(clientDTO);
         return ResponseEntity.ok(client);
     }
+    //To do implement Delete Client, Update Client, findById
 
+    //To do remove this
     //Endpoint to get a client by his CPF
     @GetMapping("/cpfCnpj/{cpfCnpj}")
     public ResponseEntity<Client> getClientByCpfCnpj(@PathVariable String cpfCnpj) {
         Optional<Client> client = clientService.getClientByCpfCnpj(cpfCnpj);
         return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    //Endpoint to get a page of filtered clients
+    @GetMapping("/findClients")
+    public Page<ClientDTO> findClients(@RequestParam(required = false) String cpfCnpj,
+                                       @RequestParam(required = false) String name,
+                                       @RequestParam(required = false) String email,
+                                       @RequestParam(required = false) String address,
+                                       @RequestParam(required = false) String phone_number,
+                                       @RequestParam(defaultValue = "0") int page,
+                                       @RequestParam(defaultValue = "10") int size,
+                                       @RequestParam(defaultValue = "name") String sortBy,
+                                       @RequestParam(defaultValue = "asac") String direction
+                                       ){
+        return clientService.findClients(cpfCnpj, name, email, address, phone_number, page, size, sortBy, direction);
+    }
+
 
 
 }
